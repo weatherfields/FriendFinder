a764d0d0318807f6b79847933a6aa55ecde1c384
Chris Cuomo <weatherfields@protonmail.com>
2019-12-16 21:06:49 -0500

    server file

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+
+// Sets up the Express App
+const app = express();
+const poRt = process.env.PORT || 5000; //if port isnt' available then run on 5000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text()); //Returns middleware that parses all bodies as a string and only looks at requests where the Content-Type header matches the type option.
+app.use(bodyParser.json({
+    type: "application/vmd.api+json"
+})); // sets type
+app.use(express.static('./app/public')); //To serve static files such as images, CSS files, and JavaScript files.
+
+require('./app/routing/htmlRoutes.js')(app)
+require('./app/routing/apiRoutes.js')(app)
+
+app.listen(poRt, () => console.log(`Server started on port ${poRt}`));
