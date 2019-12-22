7fa47fde70433f7cdf8682c31ca60267a5cd8f41
Chris Cuomo <weatherfields@protonmail.com>
2019-12-22 15:51:48 -0500

    unknown updates

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const path = require("path");
 // Sets up the Express App
 const app = express();
 const poRt = process.env.PORT || 5000; //if port isnt' available then run on 5000
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({
     extended: true
 }));
-app.use(bodyParser.text()); //Returns middleware that parses all bodies as a string and only looks at requests where the Content-Type header matches the type option.
-app.use(bodyParser.json({
-    type: "application/vmd.api+json"
-})); // sets type
+app.use(express.json());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.text()); //Returns middleware that parses all bodies as a string and only looks at requests where the Content-Type header matches the type option.
+// app.use(bodyParser.json({
+//     type: "application/vmd.api+json"
+// })); // sets type
 app.use(express.static('./app/public')); //To serve static files such as images, CSS files, and JavaScript files.
 
 require('./app/routing/htmlRoutes.js')(app)
