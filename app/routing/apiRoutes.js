bf052330a6640303fc474ee03950289d310b4638
Chris Cuomo <weatherfields@protonmail.com>
2019-12-23 19:08:22 -0500

    need to fix routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,8 +10,11 @@ module.exports = function (app) {
         res.json(friends); // api get request
     });
     // api post request
-    // when the app has a post route to friends, run function
+    // the function below will run when a user submits the form.
+    // when the app has a post route to friends, run the function
     app.post("/api/friends", function (req, res) {
-        // 
+        // this is where we compare the difference between the new users score and the other's score.
+        // I think this is where totalDifference comes into play.
+        let totalDifference = 0;
     });
 };
