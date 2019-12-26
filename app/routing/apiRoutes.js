f8dfc767dd0850e40da3622f84605d043aa0d341
Chris Cuomo <weatherfields@protonmail.com>
2019-12-26 16:10:49 -0500

    still can't get it to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,5 @@
 // link route to friend data array ...
-const friends = require("../data/friends");
-
-
-
+let friends = require("../data/friends.js");
 // setup api get request
 // gets data from json
 module.exports = function (app) {
@@ -10,11 +7,45 @@ module.exports = function (app) {
         res.json(friends); // api get request
     });
     // api post request
-    // the function below will run when a user submits the form.
+    // the function below will run when a userBody submits the form.
     // when the app has a post route to friends, run the function
     app.post("/api/friends", function (req, res) {
-        // this is where we compare the difference between the new users score and the other's score.
-        // I think this is where totalDifference comes into play.
-        let totalDifference = 0;
+        // this is where we compare the difference between the new userBodys score and the other's score.
+        // userBody fills in match variable with their own data.
+        let match = {
+            name: "",
+            photo: "",
+            difference: Infinity // global function place holder for data that will be numeric
+        }
+
+        let userBody = req.body; // req.body will hold all of the parameters the userBody sends in the POST request
+        let userBodyScores = userBody.scores; // will send back the scores parameter on the friends array.
+        let totalDifference; // initialize the totalDifference variable.
+
+        // console.log("req.body", req.body)
+        // foreach loop to loop through all of the friends in the database.
+        friends.forEach(function (object) {
+            let currentFriend = object;
+            totalDifference = 0;
+            // console.log("name", currentFriend.name)
+            // for loop to compare currentfriend scores to the other users
+            for (var i = 0; i < currentFriend.scores.length; i++) {
+
+                let currentFriendScore = currentFriend.scores[i];
+                let currentuserBodyScore = userBodyScores.scores[i];
+
+                // math.abs will return the absolute value of the number resulted from the equation below.
+                totalDifference += Math.abs(parseInt(currentFriendScore) - parseInt(currentuserBodyScore));
+            }
+            // below if statement will determine the best match for the user
+            if (totalDifference <= match.difference) {
+                match.name = currentFriend.name;
+                match.photo = currentFriend.photo;
+                match.difference = totalDifference;
+            }
+        })
+        // push the new user to the end of the friends array.
+        friends.push(userBody);
+
     });
 };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 // Sets up the Express App
 const app = express();
-const poRt = process.env.PORT || 5000; //if port isnt' available then run on 5000
+const PORT = process.env.PORT || 5000; //if port isnt' available then run on 5000
 
 // I guess bodyparser is no longer needed to return json?
 // Sets up the Express app to handle data parsing
@@ -14,11 +14,11 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-app.use(express.static('app/public/'));
+app.use(express.static('app/public'));
 
-app.use(express.static('./app/public')); //To serve static files such as images, CSS files, and JavaScript files.
+//To serve static files such as images, CSS files, and JavaScript files.
 
 require('./app/routing/htmlRoutes.js')(app);
 require('./app/routing/apiRoutes.js')(app);
 
-app.listen(poRt, () => console.log(`Server started on port ${poRt}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
